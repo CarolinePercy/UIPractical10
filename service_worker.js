4773968c3988351d93329fc864b4e28e3f300974
Caroline Percy <lineypercy@me.com>
2021-03-02 19:05:56 +0000

    Testing: adding dots to the files to cache

--- service_worker.js
+++ service_worker.js
@@ -1,18 +1,17 @@
 var cacheName = 'bikeGame-pwa';
 var filesToCache = [
-  '/',
-  '/img/ringOfFire.png',
-  '/img/Motorcycle.png',
-  '/index.html',
-  '/css/push.css',
-  '/js/script.js'
+  './',
+  './img/ringOfFire.png',
+  './img/Motorcycle.png',
+  './index.html',
+  './css/push.css',
+  './js/script.js'
 ];
 
 /* Cache contents when Offline See Cache */
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      console.log("testing if its in here");
       return cache.addAll(filesToCache);
     })
   );
